5adf34a2f94ffd53cecdc3b06d46bd0baf76a91f
ajju <ajju.me@gmail.com>
2023-03-05 12:19:58 +0000

    Reverse sentence and reverse sentence with soem special characters

---
+++ StringOperations/StringManipulations.cs
@@ -0,0 +1,43 @@
+﻿
+namespace StringOperations;
+public class StringManipulations
+{
+    public string SimpleReverseSentence(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        string output = string.Empty;
+        string[] words = input.Split(' ');
+        Array.Reverse(words);
+        output = string.Join(" ", words);
+
+        return output;
+    }
+
+    //Where are you? I am here.
+    public string ReverseSentenceWithSpecialCharacters(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        string output = string.Empty;
+
+        string[] words = input.Split(' ');
+        Array.Reverse(words);
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].EndsWith('.') || words[i].EndsWith('?') || words[i].EndsWith('!'))
+            {
+                words[i] = words[i].Substring(words[i].Length - 1) + words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, (words[i].Length - 2)).ToLower();
+            }
+            else
+            {
+                words[i]  = words[i] == "I" ? "I" : words[i].ToLower();
+            }
+        }
+        output = string.Join(" ", words);
+        return output.Trim();
+    }
+}
+
---
+++ StringOperationsTest/StringOperationsTest.cs
@@ -0,0 +1,77 @@
+﻿using StringOperations;
+
+namespace StringOperationsTest;
+
+public class StringOperationsTest
+{
+    private readonly StringManipulations _stringManipulations;
+
+    public StringOperationsTest()
+    {
+        _stringManipulations = new StringManipulations();
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void SimpleReverseWhenInputIsReversed()
+    {
+        string input = "this is a computer";
+        string output = "computer a is this";
+
+        string reverseWord = _stringManipulations.SimpleReverseSentence(input);
+        Assert.Equal(output, reverseWord);
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void SimpleReverseWhenInputIsNull()
+    {
+        string input = string.Empty;
+
+        string reverseWord = _stringManipulations.SimpleReverseSentence(input);
+        Assert.Empty(reverseWord);
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void SimpleReverseWhenInputHasCapitalLetters()
+    {
+        string input = "Hello world";
+        string output = "world Hello";
+
+        string reverseWord = _stringManipulations.SimpleReverseSentence(input);
+        Assert.Equal(output, reverseWord);
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void ReverseSentenceWithSpecialCharacters()
+    {
+        string input = "Where are you? I am here.";
+        string output = ".Here am I ?You are where";
+
+        string reverseWord = _stringManipulations.ReverseSentenceWithSpecialCharacters(input);
+        Assert.Equal(output, reverseWord);
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void ReverseSentenceWithSpecialCharactersEmpty()
+    {
+        string input = "";
+
+        string reverseWord = _stringManipulations.ReverseSentenceWithSpecialCharacters(input);
+        Assert.Empty(reverseWord);
+    }
+
+    [Fact]
+    // This test to validate if input reverses the sentence!
+    public void ReverseSentenceWithOtherSpecialCharacters()
+    {
+        string input = "What is your email id? It's a@b.com.";
+        string output = ".A@b.com it's ?Id email your is what";
+
+        string reverseWord = _stringManipulations.ReverseSentenceWithSpecialCharacters(input);
+        Assert.Equal(output, reverseWord);
+    }
+}
